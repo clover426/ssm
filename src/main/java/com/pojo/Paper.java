1ccb8c3972b901ad22219cb752cbfe028639a81c
colver426 <1351156210@qq.com>
2020-04-19 21:49:06 +0800

    ssm整合的小demo

---
+++ src/main/java/com/pojo/Paper.java
@@ -0,0 +1,60 @@
+package com.pojo;
+
+public class Paper {
+    private long paperId;
+    private String paperName;
+    private int paperNum;
+    private String paperDetail;
+
+    public long getPaperId() {
+        return paperId;
+    }
+
+    public void setPaperId(long paperId) {
+        this.paperId = paperId;
+    }
+
+    public String getPaperName() {
+        return paperName;
+    }
+
+    public void setPaperName(String paperName) {
+        this.paperName = paperName;
+    }
+
+    public int getPaperNum() {
+        return paperNum;
+    }
+
+    public void setPaperNum(int paperNum) {
+        this.paperNum = paperNum;
+    }
+
+    public String getPaperDetail() {
+        return paperDetail;
+    }
+
+    public void setPaperDetail(String paperDetail) {
+        this.paperDetail = paperDetail;
+    }
+
+    public Paper() {
+    }
+
+    public Paper(long paperId, String paperName, int paperNum, String paperDetail) {
+        this.paperId = paperId;
+        this.paperName = paperName;
+        this.paperNum = paperNum;
+        this.paperDetail = paperDetail;
+    }
+
+    @Override
+    public String toString() {
+        return "Paper{" +
+                "paperId=" + paperId +
+                ", paperName='" + paperName + '\'' +
+                ", paperNum=" + paperNum +
+                ", paperDetail='" + paperDetail + '\'' +
+                '}';
+    }
+}
